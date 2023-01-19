a8030bf0079d578fe25fa0383fe6426991cdca96
AntonMaliar <antonmaliar0@gmail.com>
2023-01-19 12:33:32 +0200

    create project

---
+++ src/test/java/anton/maliar/first/bot/FirstBotApplicationTests.java
@@ -0,0 +1,13 @@
+package anton.maliar.first.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
